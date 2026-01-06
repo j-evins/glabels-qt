7a81b93a29859f0292b96c237cb53097178f6aa8
Jaye Evins <evins@snaught.com>
2026-01-06 14:08:33 -0500

    Replaced c-style comments with c++-style comments in file banners

--- glbarcode/BarcodePostnet9.h
+++ glbarcode/BarcodePostnet9.h
@@ -1,22 +1,22 @@
-/*  BarcodePostnet9.h
- *
- *  Copyright (C) 2013  Jaye Evins <evins@snaught.com>
- *
- *  This file is part of glbarcode++.
- *
- *  glbarcode++ is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  glbarcode++ is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with glbarcode++.  If not, see <http://www.gnu.org/licenses/>.
- */
+//  BarcodePostnet9.h
+//
+//  Copyright (C) 2013  Jaye Evins <evins@snaught.com>
+//
+//  This file is part of glbarcode++.
+//
+//  glbarcode++ is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  glbarcode++ is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with glbarcode++.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef glbarcode_BarcodePostnet9_h
 #define glbarcode_BarcodePostnet9_h
