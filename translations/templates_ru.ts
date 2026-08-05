f7d2e208ff4b0b64a13876b554939e9a4e41ef61
Andrei Stepanov <adem4ik@gmail.com>
2026-08-05 09:49:20 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (107 of 107 strings)
    
    Translation: glabels-qt/templates
    Translate-URL: https://translate.fedoraproject.org/projects/glabels-qt/templates/ru/

--- translations/templates_ru.ts
+++ translations/templates_ru.ts
@@ -17,23 +17,23 @@
     </message>
     <message>
         <source>Arch file inserts</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыши для архивных папок</translation>
     </message>
     <message>
         <source>Arch file labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для архивных папок</translation>
     </message>
     <message>
         <source>Barcode labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Штрихкодовые этикетки</translation>
     </message>
     <message>
         <source>Business card CD labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для CD-визиток</translation>
     </message>
     <message>
         <source>Business card size labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки размером с визитную карточку</translation>
     </message>
     <message>
         <source>Business cards</source>
@@ -41,27 +41,27 @@
     </message>
     <message>
         <source>CD/DVD booklet</source>
-        <translation type="unfinished"></translation>
+        <translation>Буклет для CD/DVD</translation>
     </message>
     <message>
         <source>CD/DVD case (rightside up)</source>
-        <translation type="unfinished"></translation>
+        <translation>Футляр для CD/DVD (прямая ориентация)</translation>
     </message>
     <message>
         <source>CD/DVD case (upside down)</source>
-        <translation type="unfinished"></translation>
+        <translation>Футляр для CD/DVD (перевёрнутая ориентация)</translation>
     </message>
     <message>
         <source>CD/DVD case booklet</source>
-        <translation type="unfinished"></translation>
+        <translation>Буклет для футляра CD/DVD</translation>
     </message>
     <message>
         <source>CD/DVD case labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для футляра CD/DVD</translation>
     </message>
     <message>
         <source>CD/DVD center hub labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для центрального кольца CD/DVD</translation>
     </message>
     <message>
         <source>CD/DVD labels</source>
@@ -69,19 +69,19 @@
     </message>
     <message>
         <source>CD/DVD labels (disc labels)</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для CD/DVD (этикетки для дисков)</translation>
     </message>
     <message>
         <source>CD/DVD labels (disc only)</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для CD/DVD (только диск)</translation>
     </message>
     <message>
         <source>CD/DVD labels (rectangles)</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для CD/DVD (прямоугольные)</translation>
     </message>
     <message>
         <source>CD/DVD labels (spine only)</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для CD/DVD (только корешок)</translation>
     </message>
     <message>
         <source>CD/DVD or other media</source>
@@ -89,35 +89,35 @@
     </message>
     <message>
         <source>CD/DVD tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Лоток для CD/DVD</translation>
     </message>
     <message>
         <source>Candy labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для конфет</translation>
     </message>
     <message>
         <source>Cassette labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для кассет</translation>
     </message>
     <message>
         <source>Coffee and tea labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для кофе и чая</translation>
     </message>
     <message>
         <source>Correction labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Корректирующие этикетки</translation>
     </message>
     <message>
         <source>DLT labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки DLT</translation>
     </message>
     <message>
         <source>Digital media labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для цифровых носителей</translation>
     </message>
     <message>
         <source>Digital video labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для цифрового видео</translation>
     </message>
     <message>
         <source>Diskette labels</source>
@@ -129,11 +129,11 @@
     </message>
     <message>
         <source>File folder labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для файловых папок</translation>
     </message>
     <message>
         <source>Filing labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для картотек</translation>
     </message>
     <message>
         <source>Floppy disk labels</source>
@@ -153,15 +153,15 @@
     </message>
     <message>
         <source>Full face CD/DVD labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Полноформатные этикетки для CD/DVD</translation>
     </message>
     <message>
         <source>Full-page labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Полностраничные этикетки</translation>
     </message>
     <message>
         <source>Full-page labels with back slit</source>
-        <translation type="unfinished"></translation>
+        <translation>Полностраничные этикетки с задним разрезом</translation>
     </message>
     <message>
         <source>Greeting cards</source>
@@ -169,11 +169,11 @@
     </message>
     <message>
         <source>Hanging folder labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для подвесных папок</translation>
     </message>
     <message>
         <source>ID labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Идентификационные этикетки</translation>
     </message>
     <message>
         <source>Labels</source>
@@ -181,15 +181,15 @@
     </message>
     <message>
         <source>Large address labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Крупные адресные этикетки</translation>
     </message>
     <message>
         <source>Lever arch file labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для рычажных архивных папок</translation>
     </message>
     <message>
         <source>Magnet stickers</source>
-        <translation type="unfinished"></translation>
+        <translation>Магнитные наклейки</translation>
     </message>
     <message>
         <source>Mailing labels</source>
@@ -201,7 +201,7 @@
     </message>
     <message>
         <source>Medical chart labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для медицинских карт</translation>
     </message>
     <message>
         <source>Membership cards</source>
@@ -209,11 +209,11 @@
     </message>
     <message>
         <source>Metal tin container labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для металлических контейнеров</translation>
     </message>
     <message>
         <source>Mini CD/DVD labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для мини-дисков CD/DVD</translation>
     </message>
     <message>
         <source>Mini Disc labels</source>
@@ -221,23 +221,23 @@
     </message>
     <message>
         <source>Mini address labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Мини-адресные этикетки</translation>
     </message>
     <message>
         <source>Mini labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Мини-этикетки</translation>
     </message>
     <message>
         <source>Multipurpose labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Многоцелевые этикетки</translation>
     </message>
     <message>
         <source>Name badge labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для бейджей с именами</translation>
     </message>
     <message>
         <source>Name badges</source>
-        <translation type="unfinished"></translation>
+        <translation>Бейджи с именами</translation>
     </message>
     <message>
         <source>Name plates</source>
@@ -245,11 +245,11 @@
     </message>
     <message>
         <source>Nutritional labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки пищевой ценности</translation>
     </message>
     <message>
         <source>Paper hole reinforcement labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Армирующие наклейки для отверстий в бумаге</translation>
     </message>
     <message>
         <source>Passport photo labels</source>
@@ -273,7 +273,7 @@
     </message>
     <message>
         <source>Return address labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки с обратным адресом</translation>
     </message>
     <message>
         <source>Round labels</source>
@@ -285,7 +285,7 @@
     </message>
     <message>
         <source>Seal labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Печатные этикетки</translation>
     </message>
     <message>
         <source>Shipping labels</source>
@@ -297,7 +297,7 @@
     </message>
     <message>
         <source>Target stickers</source>
-        <translation type="unfinished"></translation>
+        <translation>Наклейки-мишени</translation>
     </message>
     <message>
         <source>Trapezoid labels</source>
@@ -309,15 +309,15 @@
     </message>
     <message>
         <source>Video labels (face only)</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для видео (только лицевая сторона)</translation>
     </message>
     <message>
         <source>Video tape face labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Лицевые этикетки для видеокассет</translation>
     </message>
     <message>
         <source>Video tape spine labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для корешков видеокассет</translation>
     </message>
     <message>
         <source>Zip disc labels</source>
@@ -325,71 +325,71 @@
     </message>
     <message>
         <source>Bookplate labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Библиотечные ярлыки</translation>
     </message>
     <message>
         <source>Bottle/jar labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для бутылок/банок</translation>
     </message>
     <message>
         <source>Candle labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для свечей</translation>
     </message>
     <message>
         <source>Classification labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Классификационные этикетки</translation>
     </message>
     <message>
         <source>File labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Файловые этикетки</translation>
     </message>
     <message>
         <source>VHS face labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Лицевые этикетки для VHS</translation>
     </message>
     <message>
         <source>VHS labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для VHS</translation>
     </message>
     <message>
         <source>Large arch file labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Крупные этикетки для архивных папок</translation>
     </message>
     <message>
         <source>Tube labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для туб</translation>
     </message>
     <message>
         <source>CD/DVD insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для CD/DVD</translation>
     </message>
     <message>
         <source>CD/DVD insert (back)</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для CD/DVD (задняя сторона)</translation>
     </message>
     <message>
         <source>CD/DVD insert (front)</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для CD/DVD (передняя сторона)</translation>
     </message>
     <message>
         <source>CD/DVD labels (spine labels)</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для CD/DVD (этикетки для корешков)</translation>
     </message>
     <message>
         <source>DVD insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для DVD</translation>
     </message>
     <message>
         <source>Divider labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для разделителей</translation>
     </message>
     <message>
         <source>Index cards</source>
-        <translation type="unfinished"></translation>
+        <translation>Карточки для каталога</translation>
     </message>
     <message>
         <source>Large round labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Крупные круглые этикетки</translation>
     </message>
     <message>
         <source>Post cards</source>
@@ -397,39 +397,39 @@
     </message>
     <message>
         <source>Tent cards</source>
-        <translation type="unfinished"></translation>
+        <translation>Таблички-палатки</translation>
     </message>
     <message>
         <source>VHS insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для VHS</translation>
     </message>
     <message>
         <source>VHS-C insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для VHS-C</translation>
     </message>
     <message>
         <source>Video-8 insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для Video-8</translation>
     </message>
     <message>
         <source>Window stickers</source>
-        <translation type="unfinished"></translation>
+        <translation>Оконные наклейки</translation>
     </message>
     <message>
         <source>Zip disc insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Вкладыш для Zip-диска</translation>
     </message>
     <message>
         <source>Continuous label tape</source>
-        <translation type="unfinished"></translation>
+        <translation>Непрерывная лента для этикеток</translation>
     </message>
     <message>
         <source>Postage stamp labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для почтовых марок</translation>
     </message>
     <message>
         <source>CD/DVD spine labels</source>
-        <translation type="unfinished"></translation>
+        <translation>Этикетки для корешков CD/DVD</translation>
     </message>
 </context>
 </TS>
