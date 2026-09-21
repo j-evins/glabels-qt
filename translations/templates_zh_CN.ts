2b8f4d1c087bf2ab7e32938222bcb786b585efab
hellger <mail@hexianjie.cn>
2026-09-21 12:06:34 +0800

    Update templates_zh_CN.ts

--- translations/templates_zh_CN.ts
+++ translations/templates_zh_CN.ts
@@ -5,11 +5,11 @@
     <name>XmlStrings</name>
     <message>
         <source>Address labels</source>
-        <translation>通迅录标签</translation>
+        <translation>地址标签</translation>
     </message>
     <message>
         <source>Any card</source>
-        <translation>通用名片</translation>
+        <translation>通用卡片</translation>
     </message>
     <message>
         <source>Any label</source>
@@ -17,23 +17,23 @@
     </message>
     <message>
         <source>Arch file inserts</source>
-        <translation type="unfinished"></translation>
+        <translation>档案盒插页</translation>
     </message>
     <message>
         <source>Arch file labels</source>
-        <translation type="unfinished"></translation>
+        <translation>档案盒标签</translation>
     </message>
     <message>
         <source>Barcode labels</source>
-        <translation type="unfinished"></translation>
+        <translation>条码标签</translation>
     </message>
     <message>
         <source>Business card CD labels</source>
-        <translation type="unfinished"></translation>
+        <translation>名片尺寸光盘标签</translation>
     </message>
     <message>
         <source>Business card size labels</source>
-        <translation type="unfinished"></translation>
+        <translation>名片尺寸标签</translation>
     </message>
     <message>
         <source>Business cards</source>
@@ -41,27 +41,27 @@
     </message>
     <message>
         <source>CD/DVD booklet</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 小册子</translation>
     </message>
     <message>
         <source>CD/DVD case (rightside up)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 盒（正放）</translation>
     </message>
     <message>
         <source>CD/DVD case (upside down)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 盒（倒放）</translation>
     </message>
     <message>
         <source>CD/DVD case booklet</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 盒内页</translation>
     </message>
     <message>
         <source>CD/DVD case labels</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 盒标签</translation>
     </message>
     <message>
         <source>CD/DVD center hub labels</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 中心圆标签</translation>
     </message>
     <message>
         <source>CD/DVD labels</source>
@@ -69,55 +69,55 @@
     </message>
     <message>
         <source>CD/DVD labels (disc labels)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 标签（盘片标签）</translation>
     </message>
     <message>
         <source>CD/DVD labels (disc only)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 标签（仅盘片）</translation>
     </message>
     <message>
         <source>CD/DVD labels (rectangles)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 标签（矩形款）</translation>
     </message>
     <message>
         <source>CD/DVD labels (spine only)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 标签（仅侧边）</translation>
     </message>
     <message>
         <source>CD/DVD or other media</source>
-        <translation>CD/DVD 或其它光盘</translation>
+        <translation>CD/DVD 或其它光盘介质</translation>
     </message>
     <message>
         <source>CD/DVD tray</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 托盘标签</translation>
     </message>
     <message>
         <source>Candy labels</source>
-        <translation type="unfinished"></translation>
+        <translation>糖果标签</translation>
     </message>
     <message>
         <source>Cassette labels</source>
-        <translation type="unfinished"></translation>
+        <translation>磁带标签</translation>
     </message>
     <message>
         <source>Coffee and tea labels</source>
-        <translation type="unfinished"></translation>
+        <translation>咖啡/茶叶标签</translation>
     </message>
     <message>
         <source>Correction labels</source>
-        <translation type="unfinished"></translation>
+        <translation>涂改标签</translation>
     </message>
     <message>
         <source>DLT labels</source>
-        <translation type="unfinished"></translation>
+        <translation>DLT 磁带标签</translation>
     </message>
     <message>
         <source>Digital media labels</source>
-        <translation type="unfinished"></translation>
+        <translation>数码存储介质标签</translation>
     </message>
     <message>
         <source>Digital video labels</source>
-        <translation type="unfinished"></translation>
+        <translation>数码视频标签</translation>
     </message>
     <message>
         <source>Diskette labels</source>
@@ -125,15 +125,15 @@
     </message>
     <message>
         <source>Elliptical labels</source>
-        <translation>物流标签</translation>
+        <translation>椭圆形标签</translation>
     </message>
     <message>
         <source>File folder labels</source>
-        <translation type="unfinished"></translation>
+        <translation>文件夹标签</translation>
     </message>
     <message>
         <source>Filing labels</source>
-        <translation type="unfinished"></translation>
+        <translation>归档标签</translation>
     </message>
     <message>
         <source>Floppy disk labels</source>
@@ -141,7 +141,7 @@
     </message>
     <message>
         <source>Flyer paper</source>
-        <translation type="unfinished"></translation>
+        <translation>宣传单页纸</translation>
     </message>
     <message>
         <source>Foldable business cards</source>
@@ -149,31 +149,31 @@
     </message>
     <message>
         <source>Foldable cards</source>
-        <translation>可折叠名片</translation>
+        <translation>可折叠卡片</translation>
     </message>
     <message>
         <source>Full face CD/DVD labels</source>
-        <translation type="unfinished"></translation>
+        <translation>全幅面CD/DVD标签</translation>
     </message>
     <message>
         <source>Full-page labels</source>
-        <translation type="unfinished"></translation>
+        <translation>整页标签</translation>
     </message>
     <message>
         <source>Full-page labels with back slit</source>
-        <translation type="unfinished"></translation>
+        <translation>带背缝整页标签</translation>
     </message>
     <message>
         <source>Greeting cards</source>
-        <translation>会员卡</translation>
+        <translation>贺卡</translation>
     </message>
     <message>
         <source>Hanging folder labels</source>
-        <translation type="unfinished"></translation>
+        <translation>吊夹文件夹标签</translation>
     </message>
     <message>
         <source>ID labels</source>
-        <translation type="unfinished"></translation>
+        <translation>身份标识标签</translation>
     </message>
     <message>
         <source>Labels</source>
@@ -181,15 +181,15 @@
     </message>
     <message>
         <source>Large address labels</source>
-        <translation type="unfinished"></translation>
+        <translation>大型地址标签</translation>
     </message>
     <message>
         <source>Lever arch file labels</source>
-        <translation type="unfinished"></translation>
+        <translation>拉杆夹档案标签</translation>
     </message>
     <message>
         <source>Magnet stickers</source>
-        <translation type="unfinished"></translation>
+        <translation>磁贴标签</translation>
     </message>
     <message>
         <source>Mailing labels</source>
@@ -201,7 +201,7 @@
     </message>
     <message>
         <source>Medical chart labels</source>
-        <translation type="unfinished"></translation>
+        <translation>医疗病历标签</translation>
     </message>
     <message>
         <source>Membership cards</source>
@@ -209,51 +209,51 @@
     </message>
     <message>
         <source>Metal tin container labels</source>
-        <translation type="unfinished"></translation>
+        <translation>金属罐标签</translation>
     </message>
     <message>
         <source>Mini CD/DVD labels</source>
-        <translation type="unfinished"></translation>
+        <translation>迷你CD/DVD标签</translation>
     </message>
     <message>
         <source>Mini Disc labels</source>
-        <translation>微型标签</translation>
+        <translation>迷你光盘标签</translation>
     </message>
     <message>
         <source>Mini address labels</source>
-        <translation type="unfinished"></translation>
+        <translation>迷你地址标签</translation>
     </message>
     <message>
         <source>Mini labels</source>
-        <translation type="unfinished"></translation>
+        <translation>迷你标签</translation>
     </message>
     <message>
         <source>Multipurpose labels</source>
-        <translation type="unfinished"></translation>
+        <translation>多用途标签</translation>
     </message>
     <message>
         <source>Name badge labels</source>
-        <translation type="unfinished"></translation>
+        <translation>胸牌标签</translation>
     </message>
     <message>
         <source>Name badges</source>
-        <translation type="unfinished"></translation>
+        <translation>胸牌</translation>
     </message>
     <message>
         <source>Name plates</source>
-        <translation>名称</translation>
+        <translation>铭牌/姓名牌</translation>
     </message>
     <message>
         <source>Nutritional labels</source>
-        <translation type="unfinished"></translation>
+        <translation>营养成分标签</translation>
     </message>
     <message>
         <source>Paper hole reinforcement labels</source>
-        <translation type="unfinished"></translation>
+        <translation>打孔加固贴</translation>
     </message>
     <message>
         <source>Passport photo labels</source>
-        <translation>通行证照片光滑标签</translation>
+        <translation>证件照标签</translation>
     </message>
     <message>
         <source>Photo labels</source>
@@ -261,11 +261,11 @@
     </message>
     <message>
         <source>Photo products</source>
-        <translation type="unfinished"></translation>
+        <translation>照片类产品</translation>
     </message>
     <message>
         <source>Printable mousepad</source>
-        <translation type="unfinished"></translation>
+        <translation>可打印鼠标垫</translation>
     </message>
     <message>
         <source>Rectangular labels</source>
@@ -273,7 +273,7 @@
     </message>
     <message>
         <source>Return address labels</source>
-        <translation type="unfinished"></translation>
+        <translation>回邮地址标签</translation>
     </message>
     <message>
         <source>Round labels</source>
@@ -285,7 +285,7 @@
     </message>
     <message>
         <source>Seal labels</source>
-        <translation type="unfinished"></translation>
+        <translation>封签标签</translation>
     </message>
     <message>
         <source>Shipping labels</source>
@@ -297,139 +297,139 @@
     </message>
     <message>
         <source>Target stickers</source>
-        <translation type="unfinished"></translation>
+        <translation>靶标贴纸</translation>
     </message>
     <message>
         <source>Trapezoid labels</source>
-        <translation type="unfinished"></translation>
+        <translation>梯形标签</translation>
     </message>
     <message>
         <source>Triangular labels</source>
-        <translation type="unfinished"></translation>
+        <translation>三角形标签</translation>
     </message>
     <message>
         <source>Video labels (face only)</source>
-        <translation type="unfinished"></translation>
+        <translation>录像带标签（仅正面）</translation>
     </message>
     <message>
         <source>Video tape face labels</source>
-        <translation type="unfinished"></translation>
+        <translation>录像带正面标签</translation>
     </message>
     <message>
         <source>Video tape spine labels</source>
-        <translation type="unfinished"></translation>
+        <translation>录像带侧边标签</translation>
     </message>
     <message>
         <source>Zip disc labels</source>
-        <translation>软盘标签</translation>
+        <translation>Zip 磁盘标签</translation>
     </message>
     <message>
         <source>Bookplate labels</source>
-        <translation type="unfinished"></translation>
+        <translation>藏书票标签</translation>
     </message>
     <message>
         <source>Bottle/jar labels</source>
-        <translation type="unfinished"></translation>
+        <translation>瓶/罐标签</translation>
     </message>
     <message>
         <source>Candle labels</source>
-        <translation type="unfinished"></translation>
+        <translation>蜡烛标签</translation>
     </message>
     <message>
         <source>Classification labels</source>
-        <translation type="unfinished"></translation>
+        <translation>分类标签</translation>
     </message>
     <message>
         <source>File labels</source>
-        <translation type="unfinished"></translation>
+        <translation>文件标签</translation>
     </message>
     <message>
         <source>VHS face labels</source>
-        <translation type="unfinished"></translation>
+        <translation>VHS录像带正面标签</translation>
     </message>
     <message>
         <source>VHS labels</source>
-        <translation type="unfinished"></translation>
+        <translation>VHS录像带标签</translation>
     </message>
     <message>
         <source>Large arch file labels</source>
-        <translation type="unfinished"></translation>
+        <translation>大型档案盒标签</translation>
     </message>
     <message>
         <source>Tube labels</source>
-        <translation type="unfinished"></translation>
+        <translation>管筒标签</translation>
     </message>
     <message>
         <source>CD/DVD insert</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 内页</translation>
     </message>
     <message>
         <source>CD/DVD insert (back)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 内页（背面）</translation>
     </message>
     <message>
         <source>CD/DVD insert (front)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 内页（正面）</translation>
     </message>
     <message>
         <source>CD/DVD labels (spine labels)</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 标签（侧边标签）</translation>
     </message>
     <message>
         <source>DVD insert</source>
-        <translation type="unfinished"></translation>
+        <translation>DVD 内页</translation>
     </message>
     <message>
         <source>Divider labels</source>
-        <translation type="unfinished"></translation>
+        <translation>隔页标签</translation>
     </message>
     <message>
         <source>Index cards</source>
-        <translation type="unfinished"></translation>
+        <translation>索引卡</translation>
     </message>
     <message>
         <source>Large round labels</source>
-        <translation type="unfinished"></translation>
+        <translation>大型圆形标签</translation>
     </message>
     <message>
         <source>Post cards</source>
-        <translation>贺卡/明信片</translation>
+        <translation>明信片</translation>
     </message>
     <message>
         <source>Tent cards</source>
-        <translation type="unfinished"></translation>
+        <translation>三角桌牌/台卡</translation>
     </message>
     <message>
         <source>VHS insert</source>
-        <translation type="unfinished"></translation>
+        <translation>VHS 内页</translation>
     </message>
     <message>
         <source>VHS-C insert</source>
-        <translation type="unfinished"></translation>
+        <translation>VHS-C 内页</translation>
     </message>
     <message>
         <source>Video-8 insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Video-8 内页</translation>
     </message>
     <message>
         <source>Window stickers</source>
-        <translation type="unfinished"></translation>
+        <translation>窗贴</translation>
     </message>
     <message>
         <source>Zip disc insert</source>
-        <translation type="unfinished"></translation>
+        <translation>Zip盘内页</translation>
     </message>
     <message>
         <source>Continuous label tape</source>
-        <translation type="unfinished"></translation>
+        <translation>连续标签带</translation>
     </message>
     <message>
         <source>Postage stamp labels</source>
-        <translation type="unfinished"></translation>
+        <translation>邮票标签</translation>
     </message>
     <message>
         <source>CD/DVD spine labels</source>
-        <translation type="unfinished"></translation>
+        <translation>CD/DVD 侧边标签</translation>
     </message>
 </context>
 </TS>
